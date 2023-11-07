28a8f82fa45a462174bc77c2948e2b8e415fc728
Monica-Macharia <monicamacharia7@gmail.com>
2023-11-07 10:52:52 +0300

    created the game

--- guess_two_thirds/__init__.py
+++ guess_two_thirds/__init__.py
@@ -1,86 +1,59 @@
 from otree.api import *
 
-
 doc = """
-a.k.a. Keynesian beauty contest.
-Players all guess a number; whoever guesses closest to
-2/3 of the average wins.
-See https://en.wikipedia.org/wiki/Guess_2/3_of_the_average
+Mini Ultimatum Game
 """
 
-
-class C(BaseConstants):
-    PLAYERS_PER_GROUP = 3
-    NUM_ROUNDS = 3
-    NAME_IN_URL = 'guess_two_thirds'
-    JACKPOT = cu(100)
-    GUESS_MAX = 100
-
+class Constants(BaseConstants):
+    name_in_url = 'mini_ultimatum_game'
+    players_per_group = 3
+    num_rounds = 1
+    initial_endowment = cu(200)
 
 class Subsession(BaseSubsession):
     pass
 
-
 class Group(BaseGroup):
-    two_thirds_avg = models.FloatField()
-    best_guess = models.IntegerField()
-    num_winners = models.IntegerField()
-
+    sent_amount = models.CurrencyField(
+        min=0,
+        max=Constants.initial_endowment,
+        label="How much do you want to send to Player 2?"
+    )
+    punish_decision = models.BooleanField(
+        label="Choose one:",
+        choices=[
+            [True, "Punish Player 1"],
+            [False, "Not Punish Player 1"],
+        ]
+    )
 
 class Player(BasePlayer):
-    guess = models.IntegerField(
-        min=0, max=C.GUESS_MAX, label="Please pick a number from 0 to 100:"
-    )
-    is_winner = models.BooleanField(initial=False)
+    def other_player(self):
+        return self.get_others_in_group()[0]
 
+    def set_payoffs(self):
+        if self.group.punish_decision:
+            self.payoff = cu(0)
+            self.group.get_player_by_id(1).payoff = cu(0)
+        else:
+            self.payoff = Constants.initial_endowment - self.group.sent_amount
+            self.group.get_player_by_id(1).payoff = self.group.sent_amount
 
-# FUNCTIONS
-def set_payoffs(group: Group):
-    players = group.get_players()
-    guesses = [p.guess for p in players]
-    two_thirds_avg = (2 / 3) * sum(guesses) / len(players)
-    group.two_thirds_avg = round(two_thirds_avg, 2)
-    group.best_guess = min(guesses, key=lambda guess: abs(guess - group.two_thirds_avg))
-    winners = [p for p in players if p.guess == group.best_guess]
-    group.num_winners = len(winners)
-    for p in winners:
-        p.is_winner = True
-        p.payoff = C.JACKPOT / group.num_winners
-
-
-def two_thirds_avg_history(group: Group):
-    return [g.two_thirds_avg for g in group.in_previous_rounds()]
-
-
-# PAGES
 class Introduction(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.round_number == 1
+    pass
 
+class SendMoney(Page):
+    form_model = 'group'
+    form_fields = ['sent_amount']
 
-class Guess(Page):
-    form_model = 'player'
-    form_fields = ['guess']
-
-    @staticmethod
-    def vars_for_template(player: Player):
-        group = player.group
-
-        return dict(two_thirds_avg_history=two_thirds_avg_history(group))
-
+class PunishDecision(Page):
+    form_model = 'group'
+    form_fields = ['punish_decision']
 
 class ResultsWaitPage(WaitPage):
-    after_all_players_arrive = set_payoffs
-
+    after_all_players_arrive = 'set_payoffs'
 
 class Results(Page):
-    @staticmethod
-    def vars_for_template(player: Player):
-        group = player.group
+    pass
 
-        sorted_guesses = sorted(p.guess for p in group.get_players())
-        return dict(sorted_guesses=sorted_guesses)
-
-
-page_sequence = [Introduction, Guess, ResultsWaitPage, Results]
+page_sequence = [Introduction, SendMoney, PunishDecision, ResultsWaitPage, Results]
